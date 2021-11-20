ce9a6556563a8581c2437ff287abe953e64863c6
nguyenthinga-0206 <nga3421@gmail.com>
2021-11-20 12:22:54 +0700

    ajax chatbot

--- DauGia/src/main/java/com/example/demo/controller/ChatBotController.java
+++ DauGia/src/main/java/com/example/demo/controller/ChatBotController.java
@@ -10,30 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Controller
 public class ChatBotController {
+    private final String PATH_URL_AI = "http://localhost:5000/";
+
     @RequestMapping(value = "/chatbot", method = RequestMethod.GET)
     public String chatGui() {
         return "chat/chatbot";
     }
 
-    @RequestMapping(value = "/chatbot",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String Send(@RequestParam String msg) {
-        String uri = "http://localhost:5000/send/" + msg;
-        System.out.println(uri);
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        System.out.println(result);
-        return result;
-    }
-
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     public ResponseEntity<String> SendAPI(@RequestParam String msg) {
-        String uri = "http://localhost:5000/send/" + msg;
+        String uri = PATH_URL_AI + "send/" + msg;
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         return new ResponseEntity<String>(result, HttpStatus.OK);
